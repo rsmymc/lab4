4a90f76bbfccabb28abe5ceb2db97cbd8da7ca6c
rasim <rasimyamac@hotmail.com>
2025-04-09 22:47:54 -0400

    lab4-select questions

--- lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/AbstractDBAdapter.java
+++ lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/AbstractDBAdapter.java
@@ -1,6 +1,10 @@
 package edu.canisius.cyb600.lab4.database;
 
+import edu.canisius.cyb600.lab4.dataobjects.Actor;
+import edu.canisius.cyb600.lab4.dataobjects.Film;
+
 import java.sql.Connection;
+import java.util.List;
 
 /**
  * Abstract DB Adapter
@@ -13,6 +17,11 @@ public abstract class AbstractDBAdapter {
     }
 
     //SELECTS
+    public abstract List<String> getAllDistinctCategory();
+
+    public abstract List<Film> getAllFilmsWithALengthLongerThanX(int length);
+
+    public abstract List<Actor> getActorsFirstNameStartingWithX(char firstLetter);
 
     //INSERTS
 
--- lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/PostgresDBAdapter.java
+++ lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/PostgresDBAdapter.java
@@ -1,6 +1,11 @@
 package edu.canisius.cyb600.lab4.database;
 
-import java.sql.Connection;
+import edu.canisius.cyb600.lab4.dataobjects.Actor;
+import edu.canisius.cyb600.lab4.dataobjects.Film;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Posgres Implementation of the db adapter.
@@ -9,6 +14,84 @@ public class PostgresDBAdapter extends AbstractDBAdapter {
 
     public PostgresDBAdapter(Connection conn) {
         super(conn);
+    }
+
+    @Override
+    public List<String> getAllDistinctCategory() {
+
+            try (Statement statement = conn.createStatement()) {
+
+                ResultSet results = statement.executeQuery("SELECT DISTINCT name FROM category");
+
+                List<String> categoryNames = new ArrayList<>();
+
+                while (results.next()) {
+                    categoryNames.add(results.getString("name"));
+                }
+                //Return all the films.
+                return categoryNames;
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Film> getAllFilmsWithALengthLongerThanX(int length) {
+        String sql = "Select * from film where length > ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setInt(1, length);
+            ResultSet results = statement.executeQuery();
+            List<Film> films = new ArrayList<>();
+            while (results.next()) {
+                Film film = new Film();
+                film.setFilmId(results.getInt("FILM_ID"));
+                film.setTitle(results.getString("TITLE"));
+                film.setDescription(results.getString("DESCRIPTION"));
+                film.setReleaseYear(results.getString("RELEASE_YEAR"));
+                film.setLanguageId(results.getInt("LANGUAGE_ID"));
+                film.setRentalDuration(results.getInt("RENTAL_DURATION"));
+                film.setRentalRate(results.getDouble("RENTAL_RATE"));
+                film.setLength(results.getInt("LENGTH"));
+                film.setReplacementCost(results.getDouble("REPLACEMENT_COST"));
+                film.setRating(results.getString("RATING"));
+                film.setSpecialFeatures(results.getString("SPECIAL_FEATURES"));
+                film.setLastUpdate(results.getDate("LAST_UPDATE"));
+                //Add film to the array
+                films.add(film);
+            }
+            //Return all the films.
+            return films;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Actor> getActorsFirstNameStartingWithX(char firstLetter) {
+        String sql = "Select * from actor where first_name LIKE ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, firstLetter + "%");
+            ResultSet results = statement.executeQuery();
+            List<Actor> actors = new ArrayList<>();
+            while (results.next()) {
+                Actor actor = new Actor();
+                actor.setActorId(results.getInt("actor_id"));
+                actor.setFirstName(results.getString("first_name"));
+                actor.setLastName(results.getString("last_name"));
+                actor.setLastUpdate(results.getDate("last_update"));
+                //Add film to the array
+                actors.add(actor);
+            }
+            //Return all the films.
+            return actors;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
 
